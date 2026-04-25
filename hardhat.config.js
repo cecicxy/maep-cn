d1f16d064b101e3d3b56afa5dc4c9892cf6e83c0
MAEP Project <maep@project.local>
2026-04-25 11:59:25 +0800

    chore: project scaffold, hardhat + python env

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {},
+    baseSepolia: {
+      url: process.env.BASE_SEPOLIA_RPC_URL || "https://sepolia.base.org",
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    },
+  },
+};
